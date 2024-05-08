6475264f45e6a2f6a53aca533413711ef76bd6d2
D5code <duane@design519.com>
2024-05-08 18:24:12 -0400

    Update d5-admin-tools.php

--- d5-admin-tools.php
+++ d5-admin-tools.php
@@ -4,7 +4,7 @@
  * Plugin Name: D5 Admin Tools
  * Description: A suite of utilities to assist site administration
  * Author: Duane Hass
- * Version: 0.1.75
+ * Version: 0.1.76
  *
  * This plugin updates via Git
  *
